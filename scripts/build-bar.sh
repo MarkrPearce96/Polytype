67b16790c6e2dc547f663bbfc4d564abd94ea98f
Mark Pearce <markrpearce96@gmail.com>
2026-07-10 19:54:49 +0800

    chore: bump version to 0.5 (usage calibration)

--- scripts/build-bar.sh
+++ scripts/build-bar.sh
@@ -36,8 +36,8 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
     <key>CFBundleIconFile</key><string>AppIcon</string>
     <key>CFBundleIconName</key><string>AppIcon</string>
     <key>CFBundlePackageType</key><string>APPL</string>
-    <key>CFBundleShortVersionString</key><string>0.4</string>
-    <key>CFBundleVersion</key><string>4</string>
+    <key>CFBundleShortVersionString</key><string>0.5</string>
+    <key>CFBundleVersion</key><string>5</string>
     <key>LSMinimumSystemVersion</key><string>14.0</string>
     <key>LSUIElement</key><true/>
     <key>NSPrincipalClass</key><string>NSApplication</string>
